c74ce3594a0dcee83a8f784219c630eaf51419bb
JacobWylie <jacobwylie@gmail.com>
2017-02-24 14:23:58 +0100

    Added $.ajax() method for form POST

--- js/widget.js
+++ js/widget.js
@@ -1,8 +1,24 @@
+// $('form').submit(function(evt) {
+// 	evt.preventDefault();
+// 	var url = $(this).attr("action");
+// 	var formData = $(this).serialize();
+// 	$.post(url, formData, function(response) {
+// 		$('#signup').html("<p>Thanks for signing up!</p>");
+// 	}); // end post
+// }); // end submit
+
+
+//.AJAX () below
+
 $('form').submit(function(evt) {
 	evt.preventDefault();
 	var url = $(this).attr("action");
 	var formData = $(this).serialize();
-	$.post(url, formData, function(response) {
-		$('#signup').html("<p>Thanks for signing up!</p>");
+	$.ajax(url, {
+		data : formData,
+		type : "POST",
+		success : function(response) {
+			$('#signup').html("<p>Thanks for signing up!</p>")
+		} // end success
 	}); // end post
 }); // end submit
